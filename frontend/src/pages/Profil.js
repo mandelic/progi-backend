a6006047b3cc190288e36e3f3e712b0c2ef24f23
Marija <marija.andelic02@gmail.com>
2022-11-14 23:53:25 +0100

    add frontend

---
+++ frontend/src/pages/Profil.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import NavBar from '../components/NavBar';
+
+function Profil() {
+  return (
+    <div>      <NavBar></NavBar>Profil</div>
+  )
+}
+
+export default Profil
