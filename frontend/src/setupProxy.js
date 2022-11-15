950173adee480852f9aa9b4cacc453af20b8078c
Marija <marija.andelic02@gmail.com>
2022-11-15 17:24:33 +0100

    remove proxy url

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,6 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://sahistiprogi-env.eba-qihmse7y.eu-central-1.elasticbeanstalk.com",
       changeOrigin: true,
     })
   );
