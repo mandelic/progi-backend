28160d14f234e42df5ffbeda2832111ab626d709
Marija <marija.andelic02@gmail.com>
2022-11-15 17:32:08 +0100

    add setup proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      changeOrigin: true,
+        target: "http://localhost:8080/",
+        changeOrigin: true,
     })
   );
 };
