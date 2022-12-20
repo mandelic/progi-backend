fd6f0415eb6b344af97fac2c644cb819502cfbcc
erna fiorencis <ef53298@fer.hr>
2022-12-20 14:58:57 +0100

    dodan popup u turnire

--- frontendV3/src/pages/DnevnaTaktika.js
+++ frontendV3/src/pages/DnevnaTaktika.js
@@ -8,44 +8,84 @@ import { useState }  from 'react'
 
 function DnevnaTaktika() {
 
-  const el = document.getElementById('date');
-  const dateString = new Date().toDateString();
-  el.textContent = "Taktika dana: " + dateString;
 
   let  [rjesenje, setRjesenje] = useState("");
+  let  [ocjena, setOcjena] = useState("");
+  let  [greska, setGreska] = useState("");
+
   const navigate = useNavigate();
 
   const userrjesenje = (p) => {
     setRjesenje(rjesenje = p)
   }
+  const userocjena = (p) => {
+    setOcjena(ocjena = p)
+  }
+  const usergreska = (p) => {
+    setGreska(greska = p)
+  }
+
+  async function predajOcjenu(e){
+    console.log("predajemo ocijenu")
+
+      alert("Ocjena uspješno podnesena")
+      navigate('/')
+
+  }
+
+  async function predajGresku(e){
+    console.log("predajemo grešku")
+      alert("Prijava greške uspješno podnesena")
+      navigate('/')
+
+  }
+
 
   async function predajRjesenje(e){
-    e.preventDefault();
-      fetch({
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            rjesenje: rjesenje
-        }),   
-    })
-    .then((res) => res.json())
-    .then(data => {
-      if(data.message === 'Solution correct'){
-        alert("Rješenje je ispravno")
-        navigate("/");
-      } else{
-        alert("Rješenje neispravno")
-        navigate("/");
-      }
-    });
+    console.log("predajem!")
+
+    document.getElementById('proba').innerHTML += '<div>hej!!</div>'
+  return(
+      <><><div className="form-group mt-3">
+        <label>Ocjena</label>
+        <select name="list"
+                className="form-control mt-1"
+                id='color-bg-primary' 
+                onChange={(e) => userocjena(e.target.value)}
+                required>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div><div>
+          <button type="submit" className="btn" onClick={predajOcjenu}>
+            Predaj ocjenu
+          </button>
+
+        </div></><><div className="form-group mt-3">
+          <label>Prijava greške</label>
+          <input
+            type="text"
+            className="form-control mt-1"
+            id='color-bg-primary'
+            onChange={(e) => usergreska(e.target.value)}
+            required />
+        </div><div>
+            <button type="submit" className="btn" onClick={predajGresku}>
+              Predaj grešku
+            </button>
+          </div></></>
+)
+    
   }
-  
+
+
+    
   return (
     <div>      <NavBar></NavBar>
-      <div>
+      <div id='proba'>
         <img className='slika' src={require('../images/chess.jpg')}  alt="chessboard"/>
       </div>
 
--- frontendV3/src/pages/Turnir.js
+++ frontendV3/src/pages/Turnir.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Podaci.css'
 import NavBar from '../components/NavBar';
 import Profil_NavBar from '../components/Profil_NavBar'
+import Popup from '../components/Popup';
 
 function Turnir() {
+  let  [naslov, setNaslov] = useState("");
+  let [datum, setDatum] = useState("");
+  let [lokacija, setLokacija] = useState("");
+
+  const trenernaslov = (n) => {
+    setNaslov(naslov = n)
+  }
+
+  const trenerdatum = (d) => {
+    setNaslov(datum = d)
+  }
+
+  const trenerlokacija = (l) => {
+    setNaslov(lokacija = l)
+  }
+
+  async function predajTurnir(e){
+    alert("to-do......")
+  }
+
+  let trener = true
+
+  const [isOpen, setIsOpen] = useState(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
   let prT= [{
     naslov: 'dobrotvorni turnir',
     datum: '23.4.2011',
@@ -32,7 +60,48 @@ function Turnir() {
       <NavBar></NavBar>
       <div className='podaciContainer' id='color-bg-primary'>
         <Profil_NavBar></Profil_NavBar>
-        <div>
+
+       
+
+    {isOpen && <Popup
+      content={<>
+      <div className="form-group mt-3">
+              <label>Naslov</label>
+              <input
+                type="naslov"
+                className="form-control mt-1"
+                onChange={(e) => trenernaslov(e.target.value)}
+                required
+              />
+        </div>
+        <div className="form-group mt-3">
+              <label>Datum</label>
+              <input
+                type="datum"
+                className="form-control mt-1"
+                onChange={(e) => trenerdatum(e.target.value)}
+                required
+              />
+        </div>
+        <div className="form-group mt-3">
+              <label>Lokacija</label>
+              <input
+                type="lokacija"
+                className="form-control mt-1"
+                onChange={(e) => trenerlokacija(e.target.value)}
+                required
+              />
+        </div>
+        <button type="submit" className="btn" onClick={predajTurnir} >
+                Predaj
+        </button>
+      </>}
+      handleClose={togglePopup}
+    />}
+
+
+
+        <div className='t-container'>
           <div className='t'>
 
             <table className='t-table'>
@@ -77,6 +146,11 @@ function Turnir() {
                 )
               })}
               </table>
+          </div>
+          <div className={trener ? 'trener-da' : 'trener-ne'}>
+          Pozdrav, trenere! Spreman za kreirati novi turnir?
+          <hr />
+          <button className='btn' id='trener-gumb' onClick={togglePopup}> KREIRAJ NOVI TURNIR</button>
           </div>
         </div>
       </div>
