329e7cc638b40cd5fb994e0f3514d5066b27943b
Marija <marija.andelic02@gmail.com>
2023-01-08 17:46:34 +0100

    feat: revise points if validation true

--- backend/src/main/java/com/runtimeterror/sahisti/dailyChallenge/repository/DailyChallengeGradeRepository.java
+++ backend/src/main/java/com/runtimeterror/sahisti/dailyChallenge/repository/DailyChallengeGradeRepository.java
@@ -3,8 +3,12 @@ package com.runtimeterror.sahisti.dailyChallenge.repository;
 import com.runtimeterror.sahisti.dailyChallenge.entity.DailyChallengeGrade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DailyChallengeGradeRepository extends JpaRepository<DailyChallengeGrade, Long> {
         Boolean existsDailyChallengeGradeByDailyChallengeIdAndMemberId(Long dailyChallengeId, Long memberId);
         DailyChallengeGrade findByMemberId(Long memberId);
+
+        List<DailyChallengeGrade> findAllByDailyChallengeId(Long dailyChallengeId);
 
 }
--- backend/src/main/java/com/runtimeterror/sahisti/dailyChallengeError/service/impl/DailyChallengeErrorImpl.java
+++ backend/src/main/java/com/runtimeterror/sahisti/dailyChallengeError/service/impl/DailyChallengeErrorImpl.java
@@ -3,6 +3,8 @@ package com.runtimeterror.sahisti.dailyChallengeError.service.impl;
 import com.runtimeterror.sahisti.configuration.exception.EntityIdNotFoundException;
 import com.runtimeterror.sahisti.configuration.exception.UserIdNotFoundException;
 import com.runtimeterror.sahisti.dailyChallenge.entity.DailyChallenge;
+import com.runtimeterror.sahisti.dailyChallenge.entity.DailyChallengeGrade;
+import com.runtimeterror.sahisti.dailyChallenge.repository.DailyChallengeGradeRepository;
 import com.runtimeterror.sahisti.dailyChallenge.repository.DailyChallengeRepository;
 import com.runtimeterror.sahisti.dailyChallengeError.repository.DailyChallengeErrorRepository;
 import com.runtimeterror.sahisti.dailyChallengeError.service.DailyChallengeErrorService;
@@ -26,6 +28,9 @@ public class DailyChallengeErrorImpl implements DailyChallengeErrorService {
     @Autowired
     private DailyChallengeRepository dailyChallengeRepository;
 
+    @Autowired
+    private DailyChallengeGradeRepository dailyChallengeGradeRepository;
+
     @Override
     public DailyChallengeError createNewTicket(Long userId, Long dcId, String solution, String description) {
         User member = userRepository.findById(userId).orElseThrow(() -> new UserIdNotFoundException(userId));
@@ -44,6 +49,22 @@ public class DailyChallengeErrorImpl implements DailyChallengeErrorService {
         DailyChallengeError dce = dailyChallengeErrorRepository.findById(dceId).orElseThrow(() -> new EntityIdNotFoundException("Daily challenge", dceId));
         dce.setValid(validation);
         dce.setChecked(true);
+        System.out.println(validation);
+        if (validation == true) {
+            revisePoints(dce);
+        }
         return dailyChallengeErrorRepository.save(dce);
+    }
+
+    private void revisePoints(DailyChallengeError dce) {
+        List<DailyChallengeGrade> gradeList = dailyChallengeGradeRepository.findAllByDailyChallengeId(dce.getDailyChallenge().getId());
+        gradeList.forEach(g -> {
+            if (g.getSolution().equals(dce.getSolution())) {
+                g.setPoints(1L);
+            } else {
+                g.setPoints(0L);
+            }
+        });
+        dailyChallengeGradeRepository.saveAll(gradeList);
     }
 }
