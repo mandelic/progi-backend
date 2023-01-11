06909e96b8cda122f6828a3221c3a1d1bdb6fb5d
Marija <marija.andelic02@gmail.com>
2023-01-11 13:04:39 +0100

    revert changes

--- backend/src/main/java/com/runtimeterror/sahisti/SahistiApplication.java
+++ backend/src/main/java/com/runtimeterror/sahisti/SahistiApplication.java
@@ -3,7 +3,6 @@ package com.runtimeterror.sahisti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.security.SecureRandom;
 
 @SpringBootApplication
-public class SahistiApplication extends SpringBootServletInitializer {
+public class SahistiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SahistiApplication.class, args);
